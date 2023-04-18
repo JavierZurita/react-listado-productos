4efc2673172a44167c36f48cd4a003d41ee1e881
Usuario <javierzurita95@gmail.com>
2023-04-18 22:42:19 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import HomePage from "./pages/HomePage";
+import React, { useState } from "react";
+
+export const UserContext = React.createContext("Admin");
 
 function App() {
+  const [user,setUser] = useState("Admin");
+  
+  const changeUser = () => {
+    if( user === "Usuario"){
+      setUser("Admin");
+    } else {
+      setUser("Usuario");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value={{user,setUser}}>
+       {/* AREGGLAR ESTO*/}
+        <header className={"App-header " + (user.user === "Usuario" ? "App-header blue" : "App-header black")}>
+ 
+          <div className="App-header__user">
+            <p>{user}</p>
+            <button onClick={changeUser}>CAMBIAR DE USUARIO</button>
+          </div>
+          <HomePage />
+
+        </header>
+      </UserContext.Provider>
     </div>
   );
 }
